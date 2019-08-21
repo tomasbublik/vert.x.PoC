79c029af1276993d0e3aa70348e665863b8a1c97
Tomáš Bublík <tomas.bublik@gmail.com>
2019-08-21 14:14:06 +0200

    K8S Spring vs Vert.X separation

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!bin/bash
-java -Djava.security.egd=file:/dev/./urandom -cp /app.jar -Dloader.main=$1 org.springframework.boot.loader.PropertiesLauncher
+java -Djava.security.egd=file:/dev/./urandom -Xms2048m -Xmx2048m -XX:MaxPermSize=512m -cp /app.jar -Dloader.main=$1 org.springframework.boot.loader.PropertiesLauncher
